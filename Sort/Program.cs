6a68a9b2e9f3461a7476416bf2e53507e4bb3449
wes <Wesleymiller1996@gmail.com>
2021-02-08 12:42:14 -0600

    Added sort string

---
+++ Sort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SortString("act"));
+        }
+
+        static string SortString(string s)
+        {
+            char[] schar = s.ToCharArray();
+
+            for (int i = 0; i < schar.Length; i++)
+            {
+                for (int j = 0; j < schar.Length - 1; j++)
+                {
+                    if (schar[j] > schar[j + 1])
+                    {
+                        char temp = schar[j];
+                        schar[j] = schar[j + 1];
+                        schar[j + 1] = temp;
+                    }
+                }
+            }
+
+            return new string(schar);
+        }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -9,15 +9,28 @@ namespace TicTacToe
         {
             char[,] board = IntializeBoard();
             DisplayBoard(IntializeBoard());
-            string input = "2,2";
-            int x = Int32.Parse(input.Split(',')[0]);
-            int y = Int32.Parse(input.Split(',')[1]);
-            if(IsValidMove(board, x, y) && IsValidInput(input))
-            {
-                PlaceOnBoard(ref board, 'X', x, y);
-                DisplayBoard(board);
-            }
 
+            string input = "";
+            while (input != "exit")
+            {
+                Console.Write("Please input an x,y coordinate to place your token: ");
+                input = Console.ReadLine();
+
+                if (IsValidInput(input))
+                {
+                    (int, int) pair = GetPairs(input);
+                    if (IsValidMove(board, pair.Item1, pair.Item2))
+                    {
+                        PlaceOnBoard(ref board, 'X', pair.Item1, pair.Item2);
+                        DisplayBoard(board);
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("-- Error: Invalid Input --");
+                }
+
+            }
         }
 
         static void DisplayBoard(char[,] board)
@@ -55,5 +68,6 @@ namespace TicTacToe
 
         static bool IsValidMove(char[,] board, int x, int y) => board[y, x] == '-';
         static bool IsValidInput(string input) => input.Split(',').Length == 2 && Int32.Parse(input.Split(',')[0]) < 3 && Int32.Parse(input.Split(',')[1]) < 3;
+        static (int, int) GetPairs(string input) => (Int32.Parse(input.Split(',')[0]), Int32.Parse(input.Split(',')[1]));
     }
 }

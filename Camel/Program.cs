24446bf7e676fc8dd4f11db12663f48d88d35258
wes <Wesleymiller1996@gmail.com>
2021-02-16 12:14:34 -0600

    added camel case exercise

---
+++ Camel/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+
+namespace Camel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the Cat in the hat -> theCatInTheHat
+            // the cat in the hat -> TheCatInTheHat
+
+            Console.WriteLine(CamelCase("The Cat in1 THE hat"));
+            Console.WriteLine(CamelCase("                Shadows  in   the .. Rain"));
+            Console.WriteLine(CamelCase("The Cat in THE hat"));
+            //Console.WriteLine(PascalCase("the cat in the hat"));
+        }
+
+        static string CamelCase(string input)
+        {
+            string output = "";
+            bool foundFirst = false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetterOrDigit(input[i]))
+                {
+                    if(!foundFirst)
+                    {
+                        output += input[i].ToString().ToLower();
+                        foundFirst = true;
+                    }
+                    else if (i != 0 && !(Char.IsLetterOrDigit(input[i - 1])))
+                    {
+                        output += input[i].ToString().ToUpper();
+                    } 
+                    else
+                    {
+                        output += input[i].ToString().ToLower();
+                    }
+                }
+            }
+            return output;
+        }
+
+        static string PascalCase(string input)
+        {
+            string output = "";
+            bool foundFirst = false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] >= 'A' && input[i] <= 'z')
+                {
+                    if (i != 0 && !(input[i - 1] >= 'A' && input[i - 1] <= 'z'))
+                    {
+                        output += input[i].ToString().ToUpper();
+                    }
+                    else if (i == 0 && input[i] >= 'A' && input[i] <= 'z')
+                    {
+                        output += input[i].ToString().ToUpper();
+                    }
+                    else
+                    {
+                        output += input[i].ToString().ToLower();
+                    }
+                }
+            }
+            return output;
+        }
+
+        public static string Dan (string inputString)
+        {
+            string result = "";
+            bool newWord = false;
+            foreach(char c in inputString)
+            {
+                if (char.IsWhiteSpace(c) || c == '-') newWord = true;
+                if (char.IsLetterOrDigit(c) || c == '_')
+                {
+                    result += newWord ? char.ToUpper(c) : char.ToLower(c);
+                    newWord = false;
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ PrimeCheck/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PrimeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // PRIMES
+            //2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+
+            Console.WriteLine(IsPrime(2));
+            Console.WriteLine(IsPrime(3));
+            Console.WriteLine(IsPrime(5));
+            Console.WriteLine(IsPrime(7));
+            Console.WriteLine(IsPrime(11));
+            Console.WriteLine(IsPrime(23));
+            Console.WriteLine(IsPrime(31));
+            Console.WriteLine(IsPrime(53));
+
+            // NOT PRIME
+            Console.WriteLine(IsPrime(4));
+            Console.WriteLine(IsPrime(6));
+            Console.WriteLine(IsPrime(18));
+            Console.WriteLine(IsPrime(24));
+            Console.WriteLine(IsPrime(100));
+            Console.WriteLine(IsPrime(44));
+            Console.WriteLine(IsPrime(26));
+            Console.WriteLine(IsPrime(8));
+
+            Console.WriteLine(SumOfPrimes(18));
+            Console.WriteLine(SumOfPrimes(181));
+        }
+
+        static bool IsPrime(int num)
+        {   
+            for (int i = 2; i < num - 1; i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+
+        static int SumOfPrimes(int num)
+        {
+            int sum = 0;
+            for (int i = 2; i <= num; i++)
+            {
+                sum += IsPrime(i) ? i : 0;
+            }
+            return sum;
+        }
+    }
+}
